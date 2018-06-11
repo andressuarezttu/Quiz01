638b41631b0f30420d9acaf44589373a8e0d5b5a
andressuarezttu <40004736+andressuarezttu@users.noreply.github.com>
2018-06-11 09:17:31 -0500

    matVec.py
    Added the python file containing the function

---
+++ main.py
@@ -0,0 +1,48 @@
+#Math 4330 Quiz 1
+#Andres Suarez
+
+#matVec takes a matrix and a vector and returns the product when they are multiplied.
+
+def matVec(matrix, vector):
+  '''
+Inputs:
+  matrix (3x3)
+  vector (3x1)
+Output:
+  solution (3x1)
+Details:
+  This is the implementation of the matrix by vector multiplication algorithm. 
+  
+  First we check to see if the matrix is compatible. For example if the vector has 3 elements (or rows), the matrix must have 3 lists inside of it (or 3 columns). We compare the length of the matrix with the length of the vector to determine this part.
+
+  Once we determine that the matrix is compatible, we initialize an empty array that will store our "solution". The product is calculated using item += matrix[j][i] + vector[j] and is temporarily stored in the "result" array where the sum of each product is the 3rd element in "result". The sum is then appended into our solution array which gives us a vector (3x1) equivalent to matrix*vector
+  '''
+  if len(matrix) == len(vector):
+    solution = []
+    for i in range(len(matrix)):
+      result = []
+      item = 0
+      for j in range(len(vector)):
+        item += matrix[j][i] * vector[j]
+        result.append(item)
+      solution.append(result[2])
+    return solution
+  else:
+    print("The Matrix is incompatible")
+
+    
+
+
+  #Below are my test variables which are initialized to test the function matrixProduct
+testmatrix1 = [[1,2,3],[4,5,6],[7,8,9]]
+testvector1 = [2,1,3]
+
+testvector2 = [1,2,3]
+testmatrix2 = [[1,2,3],[4,5,6],[7,8,9]]
+
+testmatrix3 = "Testing123"
+testvector3 = 3
+
+print(matVec(testmatrix1, testvector1))
+#print(matVec(testvector2, testmatrix2))
+#print(matVec(testmatrix3, testvector3))
